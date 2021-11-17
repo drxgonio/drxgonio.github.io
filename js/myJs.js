0aaaf74fbbacb6b58ca18e844927f274d32d71b4
TriPV1 <tripv1@unifiedpost.com>
2021-11-17 19:48:32 +0700

    update

--- js/myJs.js
+++ js/myJs.js
@@ -8,7 +8,7 @@ const textConfig = {
   text7: "lí do cậu thích tớ đi :vvvv",
   text8: "Gửi cho tớ <3",
   text9: "Vì cậu đẹp try vlllll",
-  text10: "Tớ biết mà ^^ Yêu cậu 300.000",
+  text10: "Tớ biết mà ^^ I love you 3000",
   text11:
     "Tối nay tớ qua đón cậu đi chơi nhaa :v Còn giờ thì chờ gì nữa mà ko inbox cho tớ đi nàooo",
   text12: "Okii lunn <3",
